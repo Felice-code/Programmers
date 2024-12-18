eab3c71bc1befff3ef805e9628db59afb9142da9
이성표 <72481151+Felice-code@users.noreply.github.com>
2024-12-18 11:26:31 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 0.13 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.py
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.py
@@ -1,7 +1,5 @@
 def solution(myString, pat):
     string, pa = myString.lower(), pat.lower()
-    if len(myString) < len(pat):
-        return 0
     if pa in string:
         return 1
     return 0
