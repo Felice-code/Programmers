d24b8e6afdee67a5d1a0bb9ad9b73a9b2902c924
이성표 <72481151+Felice-code@users.noreply.github.com>
2025-01-22 11:19:19 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
@@ -9,3 +9,12 @@ def solution(n):
         value += 3**i * int(answer[-(i+1)])
         
     return value
+
+def solution(n):
+    tmp = ''
+    while n:
+        tmp += str(n % 3)
+        n = n // 3
+
+    answer = int(tmp, 3)
+    return answer
